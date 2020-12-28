a1ec1a735cd09461e65964489cf6ba2669d5436c
Anton Vasin <mail@vasin.space>
2020-12-28 21:43:03 +0300

    fixed solution name

--- shared.cmake
+++ shared.cmake
@@ -1,8 +1,6 @@
-project(shared)
+add_library(shared INTERFACE)
 
-add_library(${PROJECT_NAME} INTERFACE)
-
-target_compile_options(${PROJECT_NAME} INTERFACE $<$<CXX_COMPILER_ID:MSVC>:/MP>)
-target_compile_options(${PROJECT_NAME} INTERFACE $<$<CXX_COMPILER_ID:MSVC>:/W3>)
-target_compile_options(${PROJECT_NAME} INTERFACE $<$<CXX_COMPILER_ID:MSVC>:/Qpar>)
-target_compile_options(${PROJECT_NAME} INTERFACE $<$<CXX_COMPILER_ID:MSVC>:/arch:AVX2>)
+target_compile_options(shared INTERFACE $<$<CXX_COMPILER_ID:MSVC>:/MP>)
+target_compile_options(shared INTERFACE $<$<CXX_COMPILER_ID:MSVC>:/W3>)
+target_compile_options(shared INTERFACE $<$<CXX_COMPILER_ID:MSVC>:/Qpar>)
+target_compile_options(shared INTERFACE $<$<CXX_COMPILER_ID:MSVC>:/arch:AVX2>)
